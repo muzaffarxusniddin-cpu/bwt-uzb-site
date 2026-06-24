f5005c65bc2d3f9e171759f15f6c498a3063a17a
abduraufrasulboyev <abduraufrasulboyev@gmail.com>
2026-06-24 20:59:37 +0500

    done

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -10,6 +10,7 @@ import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import SmoothScroll from "@/app/components/SmoothScroll";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const playfair = Playfair_Display({
   subsets: ["latin", "cyrillic"],
@@ -139,6 +140,17 @@ export default async function LocaleLayout({
       className={`${playfair.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KQHHVBZ2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NextIntlClientProvider messages={messages}>
           <SmoothScroll />
           <Header />
@@ -146,6 +158,16 @@ export default async function LocaleLayout({
           <Footer locale={locale} />
         </NextIntlClientProvider>
       </body>
+
+      {/* Google Tag Manager - Head Script */}
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KQHHVBZ2');`}
+      </Script>
+
       <GoogleAnalytics gaId="G-XYKWF44598" />
     </html>
   );
